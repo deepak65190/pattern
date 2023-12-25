555234dafbd013ca3ad65cd6ea3474d0aa444c5d
bisht65190 <bisht65190@gmail.com>
2023-12-25 13:24:48 +0530

    patterns

---
+++ patt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("------------- Z------------------------") ;
+   printf("* * * *\n") ;
+   printf("    *  \n") ;
+   printf("  *  \n") ;
+   printf("* * * *  \n") ;
+   
+    printf("------------- Z------------------------") ;
+   printf("* * * *\n") ;
+   printf("    *  \n") ;
+   printf("  *  \n") ;
+   printf("* * * *  \n") ;
+
+
+    return 0;
+}
